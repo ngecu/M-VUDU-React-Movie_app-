607f1a0dce0e3882c5e98e7f0166acd6af8f1fc0
ngecu <devngecu@gmail.com>
2023-04-14 03:13:50 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import RecipeReviewCard from './components/Card';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Row, Col,Container,Form,Navbar } from 'react-bootstrap';
+import {message,Divider} from 'antd'
 
 function App() {
+  const REACT_APP_API_KEY = process.env.REACT_APP_API_KEY
+  console.log("my key is ",REACT_APP_API_KEY);
+  // const [prompt,setPrompt] =useState("")
+  const [movies,setMovies] = useState([])
+  const [messageApi, contextHolder] = message.useMessage();
+
+ const fetchData =async (prompt) => {
+  if (prompt) {
+    const {data} = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${REACT_APP_API_KEY}&query="${prompt}`);  
+    const {results} = data 
+    setMovies(results);
+  }
+  else{
+    const {data} = await axios.get(`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${REACT_APP_API_KEY}&page=${Math.floor(Math.random() * 100) + 1}`);       
+    const {results} = data
+    console.log(results);
+    setMovies(results);
+  }
+ }
+
+
+   useEffect(()=>{
+    fetchData();  
+  },[])
+
+  const success = () => {
+    messageApi.open({
+      type: 'loading',
+      content: 'Action in progress..',
+      duration: 0,
+    });
+    // Dismiss manually and asynchronously
+    setTimeout(messageApi.destroy, 2500);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App ">
+      <Container maxWidth="sm">
+      {contextHolder}
+
+      <Navbar bg="dark" variant="dark" fixed="top">
+      <Container>
+        <Navbar.Brand href="#home" style={{"color":"white"}}>
+        <img
+              src="https://i.pinimg.com/originals/4d/78/79/4d7879049736de0fc394cb31686bb6c9.gif"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+            />
+          
+          M-VUDU</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+          <Form.Group className="mb-3" controlId="formBasicMovie">
+        <Form.Control type="text" placeholder="Enter Movie Name" onKeyUp={(e)=>{
+          fetchData(e.target.value)
+          success()
+        }} />
+      </Form.Group>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+     
+
+<Row style={{marginTop:"7%"}}>
+{movies.map((item,i)=>(
+    <Col xs={3} lg={3}>
+         <RecipeReviewCard item={item} />
+    </Col>
+     ))}
+    <Divider />
+  </Row>
+
+       
+      </Container>
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,91 @@
+import * as React from 'react';
+import { useState } from 'react';
+import { Card } from 'antd';
+import { Drawer } from 'antd';
+import { Row,Col } from 'react-bootstrap';
+import StarRatings from 'react-star-ratings';
+
+
+const { Meta } = Card;
+
+
+export default function RecipeReviewCard({item}) {
+  const rating = item.vote_average
+
+  const [open, setOpen] = useState(false);
+  const [drawerImg, setDrawerimg] = useState('');
+  const [drawerTitle, setDrawertitle] = useState('');
+  const [vote_average,setVoteavreage] = useState('');
+  const [vote_count,setvoteCount] = useState('')
+  const [overview,setOverview] = useState('')
+  const [release_date,setReleasedate] = useState('')
+
+  
+
+
+  const showDrawer = () => {
+    setOpen(true);
+  };
+
+  const onClose = () => {
+    setOpen(false);
+  };
+
+  return(
+    <>
+    <Card
+    
+    onMouseOver={
+      ()=>{
+      showDrawer()
+      setDrawerimg(`https://image.tmdb.org/t/p/w1280${item.poster_path}`)
+      setDrawertitle(item.title)
+      setVoteavreage(item.vote_average)
+      setvoteCount(item.vote_count)
+      setOverview(item.overview)
+      setReleasedate(item.release_date)
+
+    }}
+    cover={<img alt="example" src={`https://image.tmdb.org/t/p/w1280${item.poster_path}`} />}
+  >
+    <Meta title={item.title} description={<p style={{"color":"#C28BFF"}} >
+    <StarRatings
+        rating={(rating*5)/10}
+        starRatedColor="purple"
+        numberOfStars={5}
+        starDimension="15px"
+        starSpacing="1px"
+      />
+      ({item.vote_count} Votes)
+       </p>} />
+  </Card>
+
+  <Drawer title={drawerTitle} placement="right" onClose={onClose} open={open}>
+        <Row>
+          <Col>
+          <img alt="example" style={{width:"100%"}} src={drawerImg} />
+          </Col>
+        </Row>
+       <h1>Overview</h1>
+        <p>{overview}</p>
+        <p>Rating : 
+          
+        <StarRatings
+        rating={(rating*5)/10}
+        starRatedColor="purple"
+        numberOfStars={5}
+        starDimension="20px"
+        starSpacing="1px"
+      />
+          </p>
+        <p>Vount Count : {vote_count}</p>
+        <p>Release Date : {release_date}</p>
+
+
+        
+
+
+      </Drawer>
+  </>
+  )
+}
